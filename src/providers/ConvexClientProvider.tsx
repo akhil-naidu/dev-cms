a51bb8153adc5c9ec51e003fc1f119ba9c53d005
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-04-20 00:46:56 +0530

    chore: typesafe env variables

--- src/providers/ConvexClientProvider.tsx
+++ src/providers/ConvexClientProvider.tsx
@@ -3,7 +3,9 @@
 import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import { ReactNode } from 'react'
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!) // skipcq: JS-0339
+import { env } from '@/env'
+
+const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL)
 
 export default function ConvexClientProvider({
   children,
