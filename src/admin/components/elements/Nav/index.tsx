6847e2a516117c24dabfe75eacd30662d3bbe4ff
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-04-19 16:45:26 +0530

    chore: added table

--- src/admin/components/elements/Nav/index.tsx
+++ src/admin/components/elements/Nav/index.tsx
@@ -25,7 +25,7 @@ const DefaultNav: React.FC<Props> = props => {
     <div className='hidden border-r bg-muted/40 md:block'>
       <div className='flex h-full max-h-screen flex-col gap-2'>
         <div className='flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6'>
-          <Link className='flex items-center gap-2 font-semibold' href='#'>
+          <Link className='flex items-center gap-2 font-semibold' href='/admin'>
             <Package2Icon className='h-6 w-6' />
             <span className=''>Dev Cms</span>
           </Link>
--- src/app/(cms)/admin/collections/[slug]/page.tsx
+++ src/app/(cms)/admin/collections/[slug]/page.tsx
@@ -1,7 +1,31 @@
-import ListView from '@/admin/components/views/collections/List'
+import Header from '@/admin/components/elements/Header'
+import { columns } from '@/admin/components/elements/table/columns'
+import { DataTable } from '@/admin/components/elements/table/data-table'
+import { UserNav } from '@/admin/components/elements/table/user-nav'
+import { tableData } from '@/admin/data/table/tasks'
 
-const Edit: React.FC = () => {
-  return <ListView />
+const TaskPage = () => {
+  const tasks = tableData
+
+  return (
+    <div className='overflow-hidden flex flex-col'>
+      <Header />
+      <div className='h-full flex-1 flex-col space-y-8 p-8 md:flex'>
+        <div className='flex items-center justify-between space-y-2'>
+          <div>
+            <h2 className='text-2xl font-bold tracking-tight'>Welcome back!</h2>
+            <p className='text-muted-foreground'>
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          <div className='flex items-center space-x-2'>
+            <UserNav />
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </div>
+  )
 }
 
-export default Edit
+export default TaskPage
