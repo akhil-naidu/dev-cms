c43c654a792e1275d8db348b94f96791dec2747b
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-04-22 10:10:18 +0530

    chore: updated the pages

--- src/admin/components/views/collections/List/index.tsx
+++ src/admin/components/views/collections/List/index.tsx
@@ -1,16 +1,24 @@
 import NoCollections from '@/admin/components/elements/EmptyStates/NoCollections'
-import Header from '@/admin/components/elements/Header'
+import { columns } from '@/admin/components/elements/Table/columns'
+import { DataTable } from '@/admin/components/elements/Table/data-table'
 import { collections } from '@/admin/data/collections'
-import { Collections } from '@/admin/data/collections/types'
-
-import DefaultList from './Default'
+import { tableData } from '@/admin/data/table/tasks'
 
 const ListView: React.FC = () => {
   return (
-    <div className='flex flex-col'>
-      <Header />
+    // skipcq: JS-0415
+
+    <div className='h-full flex-1 flex-col space-y-8 p-8 md:flex'>
+      <div className='flex items-center justify-between space-y-2'>
+        <div>
+          <h2 className='text-2xl font-bold tracking-tight'>Welcome back!</h2>
+          <p className='text-muted-foreground'>
+            Here&apos;s a list of your tasks for this month!
+          </p>
+        </div>
+      </div>
       {collections.length ? (
-        <DefaultList collections={collections as Collections} />
+        <DataTable data={tableData} columns={columns} />
       ) : (
         <NoCollections />
       )}
