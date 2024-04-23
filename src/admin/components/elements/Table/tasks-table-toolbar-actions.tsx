a805421318defc1fb85285b743a851b09cf87aa8
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-04-23 11:21:10 +0530

    chore: tasks table toolbar actions

--- src/admin/components/elements/Table/tasks-table-toolbar-actions.tsx
+++ src/admin/components/elements/Table/tasks-table-toolbar-actions.tsx
@@ -1,42 +1,43 @@
-"use client"
+'use client'
 
-import { type Task } from "@/db/schema"
-import { DownloadIcon } from "@radix-ui/react-icons"
-import { type Table } from "@tanstack/react-table"
+import { DownloadIcon } from '@radix-ui/react-icons'
+import { type Table } from '@tanstack/react-table'
 
-import { exportTableToCSV } from "@/lib/export"
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
+import { Doc } from '@/convex/_generated/dataModel'
 
-import { CreateTaskDialog } from "./create-task-dialog"
-import { DeleteTasksDialog } from "./delete-tasks-dialog"
+import { CreateTaskDialog } from './create-task-dialog'
+import { DeleteTasksDialog } from './delete-tasks-dialog'
+import { exportTableToCSV } from './lib/export'
 
 interface TasksTableToolbarActionsProps {
-  table: Table<Task>
+  table: Table<Doc<'task'>>
 }
 
 export function TasksTableToolbarActions({
   table,
 }: TasksTableToolbarActionsProps) {
   return (
-    <div className="flex items-center gap-2">
+    <div className='flex items-center gap-2'>
       {table.getFilteredSelectedRowModel().rows.length > 0 ? (
         <DeleteTasksDialog
           tasks={table.getFilteredSelectedRowModel().rows}
+          // skipcq: JS-0417
           onSuccess={() => table.toggleAllPageRowsSelected(false)}
         />
       ) : null}
-      <CreateTaskDialog prevTasks={table.getFilteredRowModel().rows} />
+      <CreateTaskDialog />
       <Button
-        variant="outline"
-        size="sm"
+        variant='outline'
+        size='sm'
+        // skipcq: JS-0417
         onClick={() =>
           exportTableToCSV(table, {
-            filename: "tasks",
-            excludeColumns: ["select", "actions"],
+            filename: 'tasks',
+            excludeColumns: ['select', 'actions'],
           })
-        }
-      >
-        <DownloadIcon className="mr-2 size-4" aria-hidden="true" />
+        }>
+        <DownloadIcon className='mr-2 size-4' aria-hidden='true' />
         Export
       </Button>
       {/**
