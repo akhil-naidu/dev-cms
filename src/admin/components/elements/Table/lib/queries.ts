751b8656d4c3c76c5954adc39b8bd4fd47e639b4
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-04-24 11:34:42 +0530

    chore: added get query

--- src/admin/components/elements/Table/lib/queries.ts
+++ src/admin/components/elements/Table/lib/queries.ts
@@ -1,19 +1,29 @@
+'use server'
+
+import { fetchQuery } from 'convex/nextjs'
 import { unstable_noStore as noStore } from 'next/cache'
 
-import { Doc } from '@/convex/_generated/dataModel'
+import { api } from '@/convex/_generated/api'
 
 import { tasksData } from './data'
+import { GetTasksSchema } from './validations'
 
-export async function getTasks() {
-  // input: GetTasksSchema
+export async function getTasks(input: GetTasksSchema) {
   noStore()
-  // const { page, per_page, sort, title, status, priority, operator, from, to } =
-  //   input
+  // skipcq: JS-0356
+  const { page, per_page, sort, title, status, priority, operator, from, to } =
+    input
 
   try {
-    const data = (await tasksData) as Doc<'task'>[]
-    const pageCount = 5
-    return { data, pageCount }
+    // const data = (await tasksData) as Doc<'task'>[]
+    const data = await fetchQuery(api.task.paginate, {
+      paginationOpts: {
+        numItems: per_page,
+        cursor: null,
+      },
+    })
+
+    return { data: data.page, pageCount: per_page }
   } catch (err) {
     return { data: [], pageCount: 0 }
   }
