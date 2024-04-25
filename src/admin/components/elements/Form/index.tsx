b44e73c2f9b02bfd0a799bb1086ed110d7c0e059
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-04-25 15:22:21 +0530

    chore: added create and update actions

--- src/admin/components/elements/Form/index.tsx
+++ src/admin/components/elements/Form/index.tsx
@@ -1,12 +1,25 @@
 'use client'
 
+import { LoaderIcon } from '../../icons'
+import { createTask } from '../Table/lib/actions'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { usePathname, useRouter } from 'next/navigation'
+import { useTransition } from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
+import { updateTask } from '@/admin/components/elements/Table/lib/actions'
+import {
+  CreateTaskSchema,
+  type UpdateTaskSchema,
+  updateTaskSchema,
+} from '@/admin/components/elements/Table/lib/validations'
 import AutoForm, { AutoFormSubmit } from '@/components/auto-form'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
-import { Doc } from '@/convex/_generated/dataModel'
+import { Doc, Id } from '@/convex/_generated/dataModel'
 import { Task_Zod_Object } from '@/convex/task'
+import { getErrorMessage } from '@/utils/handle-error'
 
 interface Props {
   task?: Doc<'task'>
@@ -18,6 +31,61 @@ const Form: React.FC<Props> = ({ task }) => {
   const router = useRouter()
 
   const isCreatePage = pathname.split('/').pop() === 'create'
+
+  const [isCreatePending, startCreateTransition] = useTransition()
+  const [isUpdatePending, startUpdateTransition] = useTransition()
+
+  const form = useForm<UpdateTaskSchema>({
+    resolver: zodResolver(updateTaskSchema),
+    defaultValues: {
+      title: task?.title ?? '',
+      label: task?.label,
+      status: task?.status,
+      priority: task?.priority,
+    },
+  })
+
+  const handleUpdate = (input: UpdateTaskSchema) => {
+    console.log('input: ', input)
+    startUpdateTransition(() => {
+      toast.promise(
+        updateTask({
+          id: task?._id as Id<'task'>,
+          ...input,
+        }),
+        {
+          loading: 'Updating task...',
+          success: () => {
+            return 'Task updated'
+          },
+          error: error => {
+            return getErrorMessage(error)
+          },
+        },
+      )
+    })
+  }
+
+  const handleCreate = (input: CreateTaskSchema) => {
+    startCreateTransition(() => {
+      toast.promise(
+        createTask({
+          ...input,
+        }),
+        {
+          loading: 'Creating task...',
+          success: data => {
+            form.reset()
+            router.push(data?.data?._id || '../')
+            return 'Task created'
+          },
+          error: error => {
+            return getErrorMessage(error)
+          },
+        },
+      )
+    })
+  }
 
   return task?._id || isCreatePage ? (
     // skipcq: JS-0415
@@ -32,8 +100,28 @@ const Form: React.FC<Props> = ({ task }) => {
           <div className='space-y-6'>
             <Separator />
             {/* <EditForm task={task} /> */}
-            <AutoForm formSchema={Task_Zod_Object} values={task}>
-              <AutoFormSubmit>Update</AutoFormSubmit>
+            <AutoForm
+              formSchema={Task_Zod_Object}
+              values={task}
+              // skipcq: JS-0417
+              onSubmit={isCreatePage ? handleCreate : handleUpdate}>
+              {isCreatePage ? (
+                <AutoFormSubmit>
+                  {isCreatePending ? (
+                    <LoaderIcon className='animate-spin h-5 w-5' />
+                  ) : (
+                    'Create'
+                  )}
+                </AutoFormSubmit>
+              ) : (
+                <AutoFormSubmit>
+                  {isUpdatePending ? (
+                    <LoaderIcon className='animate-spin h-5 w-5' />
+                  ) : (
+                    'Update'
+                  )}
+                </AutoFormSubmit>
+              )}
             </AutoForm>
           </div>
         </div>
